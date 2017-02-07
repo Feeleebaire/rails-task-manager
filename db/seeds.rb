03f2364b7296298ca921fe027bc66aac7debc2ed
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-02-07 12:35:51 +0100

    task_manager_done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Task.destroy_all
+
+Task.create(title: 'My first task', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita earum a dolores repellendus enim error provident modi quas, dolor necessitatibus ipsam ullam delectus ab labore numquam tenetur, veniam natus possimus.', status: 'TODO')
+Task.create(title: 'My second task', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita earum a dolores repellendus enim error provident modi quas, dolor necessitatibus ipsam ullam delectus ab labore numquam tenetur, veniam natus possimus.', status: 'TODO')
+Task.create(title: 'My third task', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita earum a dolores repellendus enim error provident modi quas, dolor necessitatibus ipsam ullam delectus ab labore numquam tenetur, veniam natus possimus.', status: 'TODO')
+Task.create(title: 'My last task', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita earum a dolores repellendus enim error provident modi quas, dolor necessitatibus ipsam ullam delectus ab labore numquam tenetur, veniam natus possimus.', status: 'TODO')
